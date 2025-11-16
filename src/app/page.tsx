11ef671dc3d6000af030acb51c2317c09b70f259
hyejun <hyejunkkim228@gmail.com>
2025-11-17 00:15:04 +0900

    feat: 파비콘 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,6 @@ export default function Home() {
         </div>
       </section>
 
-      {/* 메인 컨텐츠 */}
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
           
