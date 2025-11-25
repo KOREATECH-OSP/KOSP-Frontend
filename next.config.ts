ad78817d185242b8d591ef494505550bfd8b9c38
박태진 <140797244+taejinn@users.noreply.github.com>
2025-11-25 21:39:34 +0900

    fix: 개발 서버 실행 오류

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
