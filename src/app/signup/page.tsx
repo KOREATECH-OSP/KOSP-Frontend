87ac36ea4dab3e6d56d6c7d17fe1ecc5535dbc25
hyejun <hyejunkkim228@gmail.com>
2025-11-27 22:51:09 +0900

    chore: 높이 수정

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -62,8 +62,8 @@ export default function SignupPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col">
-      <div className="flex-1 flex justify-center pt-30 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-[calc(100vh-60px)] bg-gray-50 flex flex-col">
+      <div className="flex-1 flex justify-center pt-20 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div className="text-center">
             <h2 className="text-3xl font-bold text-gray-900">회원가입</h2>
