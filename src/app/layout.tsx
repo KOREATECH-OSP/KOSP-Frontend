211c2a1c6c8d8433abd638092e21a659931e7d74
박태진 <140797244+taejinn@users.noreply.github.com>
2026-02-06 22:55:42 +0900

    feat: Google Tag Manager(GTM-N8KGQ37N) 설치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Toaster } from "sonner";
@@ -18,6 +19,17 @@ export default async function RootLayout({
   return (
     <html lang="ko">
       <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N8KGQ37N');`,
+          }}
+        />
         <link
           rel="stylesheet"
           as="style"
@@ -26,6 +38,14 @@ export default async function RootLayout({
         />
       </head>
       <body className={`antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N8KGQ37N"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Providers initialSession={session}>{children}</Providers>
         <Toaster
           richColors
